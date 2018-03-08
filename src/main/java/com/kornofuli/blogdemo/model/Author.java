3da83a113f7cc85426657385174e9ad9a110e0bc
AvishekBarua <avishek_barua@yahoo.com>
2018-03-08 12:19:27 +0200

    author model created.

--- src/main/java/com/kornofuli/blogdemo/model/Author.java
+++ src/main/java/com/kornofuli/blogdemo/model/Author.java
@@ -1,5 +1,86 @@
 package com.kornofuli.blogdemo.model;
 
-public class Author {
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name="author")
+public class Author {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	//@Column(name = "author_id")
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String password;
+
+	
+	@OneToMany(mappedBy="author", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonManagedReference
+	private Set<Post> posts; 
+	
+	public Author() {
+	}
+
+	
+	public Author(String firstName, String lastName, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setSecondName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public Set<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(Set<Post> posts) {
+		this.posts = posts;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", secondName=" + lastName + ", password=" + password
+				+ "]";
+	}
 }
+
